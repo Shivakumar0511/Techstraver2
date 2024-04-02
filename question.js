9c27b3d76e884d7d230f4e226b6364ea94e65b10
Shivakumar0511 <160015501+Shivakumar0511@users.noreply.github.com>
2024-04-02 10:00:39 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,41 @@
+const questions=[
+    {
+         "id":1,
+         "question":"which of the following can read and render HTML web pages?",
+         "options":["server", "Head Tak","Web browser","Empty"],
+         "answer": 2
+     },
+     {
+         "id":2,
+         "question":"The latest HTML standard is?",
+         "options":["HTML 4.0", "HTML 5.0","XML", "SGML"],
+         "answer": 1
+     },
+     {
+         "id":3,
+         "question":"What are variables used in JavaScript programs?",
+         "options":["Varying randomly", "Storing numbers,dates,and other values","Used as header files", "None of the aboveS"],
+         "answer": 1
+     },
+     {
+         "id":4,
+         "question":"On which model is www based upon ?",
+         "options":["Client-server", "Local-server","3 tier", "None"],
+         "answer": 0
+     },
+     {
+         "id":3,
+         "question":"Among which of the following is HTML a subset?",
+         "options":["SGMD", "SGMT","SGML", "None"],
+         "answer": 2
+     },
+     {
+         "id":3,
+         "question":"Which of the following the attribute is used for merging two or more adjacent columns?",
+         "options":["Rowspan", "Cellspacing","Colspan", "Cellpadding"],
+         "answer": 2
+     }
+ ]
+ 
+ 
+ 
